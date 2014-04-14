158c29d4973beed9b47adc171bbf08e8346da76c
Anibal Copitan <acopitan@gmail.com>
2014-04-14 18:55:36 -0500

    slide

---
+++ application/views/layout/layout-partial/slider.php
@@ -0,0 +1,106 @@
+            <!--Slider-->
+            <div class="row clearfix">
+                <div class="col-md-12" id="esliderWeb">
+                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner borderRadiusButtom" id="esliderWeb">
+
+                            <div class="item">
+                                <img src="<?php echo getPublicUrl() ?>/images/banner/1.jpg" width="1001" height="504" />
+                            </div>
+
+                            <div class="item">
+                                <img src="<?php echo getPublicUrl() ?>/images/banner/1.jpg" width="1001" height="504" />
+                            </div>
+
+                            <div class="item">
+                                <img src="<?php echo getPublicUrl() ?>/images/banner/1.jpg" width="1001" height="504" />
+                            </div>
+
+                        </div>
+
+                        <!-- Controls -->
+                        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                        </a>
+                    </div>
+                </div>
+            </div><!--End Slider--> 
+
+            <!--Tab Center-->
+            <div class="row clearfix">
+                <div class="col-md-12">
+
+                    <div class="tabCustom">
+                        <!-- Nav tabs -->
+                        <ul class="nav nav-tabs" id="ulTab">
+                            <li class="active" style="background:url(<?php echo getPublicUrl() ?>/images/tab/tab_border_right.jpg) right 10px no-repeat;"><a href="#one" data-toggle="tab">Home</a></li>
+                            <li><a href="#two" data-toggle="tab">Populars</a></li>
+                        </ul>
+
+                        <!-- Tab panes -->
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="one">
+
+                                <!--Item Tab-->
+                                <div class="tabShowRell">
+                                    <img src="<?php echo getPublicUrl() ?>/images/showreel/image.jpg" class="img-responsive" width="218" height="200" />
+                                </div>
+                                <!--End Item Tab-->
+                                <!--Item Tab-->
+                                <div class="tabShowRell">
+                                    <img src="<?php echo getPublicUrl() ?>/images/showreel/image.jpg" class="img-responsive" width="218" height="200" />
+                                </div>
+                                <!--End Item Tab-->
+                                <!--Item Tab-->
+                                <div class="tabShowRell">
+                                    <img src="<?php echo getPublicUrl() ?>/images/showreel/image.jpg" class="img-responsive" width="218" height="200" />
+                                </div>
+                                <!--End Item Tab-->
+                                <!--Item Tab-->
+                                <div class="tabShowRell">
+                                    <img src="<?php echo getPublicUrl() ?>/images/showreel/image.jpg" class="img-responsive" width="218" height="200" />
+                                </div>
+                                <!--End Item Tab-->
+
+                            </div>
+                            <div class="tab-pane" id="two">
+
+                                <!--Item Tab-->
+                                <div class="tabShowRell">
+                                    <img src="<?php echo getPublicUrl() ?>/images/showreel/image.jpg" class="img-responsive" width="218" height="200" />
+                                </div>
+                                <!--End Item Tab-->
+                                <!--Item Tab-->
+                                <div class="tabShowRell">
+                                    <img src="<?php echo getPublicUrl() ?>/images/showreel/image.jpg" class="img-responsive" width="218" height="200" />
+                                </div>
+                                <!--End Item Tab-->
+                                <!--Item Tab-->
+                                <div class="tabShowRell">
+                                    <img src="<?php echo getPublicUrl() ?>/images/showreel/image.jpg" class="img-responsive" width="218" height="200" />
+                                </div>
+                                <!--End Item Tab-->
+                                <!--Item Tab-->
+                                <div class="tabShowRell">
+                                    <img src="<?php echo getPublicUrl() ?>/images/showreel/image.jpg" class="img-responsive" width="218" height="200" />
+                                </div>
+                                <!--End Item Tab-->
+
+                            </div>
+                        </div>
+                        <!-- END Nav tabs -->
+                    </div>
+                </div>
+            </div>
+            <!--End Tab Center--> 
