7c5b9e6ac09406c2feba7682852b2f5b0b512e96
Anibal Copitan <acopitan@gmail.com>
2014-04-15 01:09:10 -0500

    data with cache

--- application/models/banner_model.php
+++ application/models/banner_model.php
@@ -18,11 +18,42 @@ class Banner_model  extends CI_Model {
      */
     public function listBanner($status = 1, $limit = 5)
     {   
-        $this->db->select()->from($this->_name);
-        $this->db->where("status = $status");
-        $this->db->limit($limit);
-        $query = $this->db->get();        
-        return $query->result_array(); 
+        $keyCache = __CLASS__ . __FUNCTION__ .'_'. $status.$limit;
+        
+        if (($rs = $this->cache->file->get($keyCache)) == false ) {
+            $this->db->select()->from($this->_name);
+            $this->db->where("status = $status");
+            $this->db->limit($limit);
+            $query = $this->db->get();
+            $rs = $query->result_array();
+            
+            $this->cache->file->save($keyCache, $rs, 600);
+        }
+        return $rs;
     }
+    
+    /**
+     * List of banner Random.
+     * @param type $status
+     * @param type $limit
+     * @return type
+     */
+    public function listPopular($status = 1, $limit = 5)
+    {   
+        $keyCache = __CLASS__ . __FUNCTION__ .'_'. $status.$limit;
+        
+        if (($rs = $this->cache->file->get($keyCache)) == false ) {
+            $this->db->select()->from($this->_name);
+            $this->db->where("status = $status");
+            $this->db->order_by('id', 'random');
+            $this->db->limit($limit);
+            $query = $this->db->get();        
+            $rs = $query->result_array();
+            
+            $this->cache->file->save($keyCache, $rs, 600);
+        }        
+        return $rs;
+    }    
+    
     
 }
