a9c8e6902b34a73c67d514607dca7a82fd858a5d
Anibal Copitan <acopitan@gmail.com>
2014-04-22 02:31:27 -0500

    add css custom

--- application/views/layout-admin/layout.php
+++ application/views/layout-admin/layout.php
@@ -8,25 +8,25 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>My Panel</title>
-        <?php //require 'layout-partial/require_css.php'; ?>
+    <?php //require 'layout-partial/require_css.php'; ?>
 
     <link rel="stylesheet" type="text/css" href="<?php echo getPublicUrl() ?>/css/admin/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="<?php echo getPublicUrl() ?>/css/admin/style.css">
 
-<!--[if lt IE 9]>
-        <script src="<?php echo getPublicUrl() ?>/js/html5.js"></script>
-<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
+    <!--[if lt IE 9]>
+            <script src="<?php echo getPublicUrl() ?>/js/html5.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-<!--Menu-->
-<link rel="stylesheet" type="text/css" href="<?php echo getPublicUrl() ?>/menuElegante/css/menu.css">
-<link href="<?php echo getPublicUrl() ?>/menuElegante/css/maps.css" rel="stylesheet" />
-<!--Menu-->
+    <!--Menu-->
+    <link rel="stylesheet" type="text/css" href="<?php echo getPublicUrl() ?>/menuElegante/css/menu.css">
+    <link href="<?php echo getPublicUrl() ?>/menuElegante/css/maps.css" rel="stylesheet" />
+    <!--Menu-->
 
-<!--Editor-->
-<link rel="stylesheet" type="text/css" href="<?php echo getPublicUrl() ?>/editor/bootstrap/bootstrap_extend.css">
-        <!--<link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>-->
-<!--Editor-->
+    <!--Editor-->
+    <link rel="stylesheet" type="text/css" href="<?php echo getPublicUrl() ?>/editor/bootstrap/bootstrap_extend.css">
+            <!--<link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>-->
+    <!--Editor-->
 
     </head>
 
